0ec459d07c6e9165bc12a30247b5c02d74ed4f5c
AbdullahShahid7402 <abdullahshahid210@gmail.com>
2024-12-03 20:33:58 +0500

    bat animation added for still frames

--- Assets/Scripts/IceBats.cs
+++ Assets/Scripts/IceBats.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IceBats : MonoBehaviour
 {
+    // vision of the bat
     public CircleCollider2D vision;
+    // initial position of the bat
+    private Vector2 startPosition;
+    // this is the Animator on the bat to make sure its animations
+    private Animator bat_animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bat_animator = this.GetComponent<Animator>();
+        startPosition = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D()
+    {
+
     }
 }
